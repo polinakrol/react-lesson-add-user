7bbbf9214e376649939ba55898b8b41799406571
Polina <polinak@liveu.tv>
2022-10-13 15:33:13 +0300

    forth commit

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -5,11 +5,13 @@ import Card from './Card';
 import Button from './Button';
 import classes from './ErrorModal.module.css';
 
-const ErrorModal = (props) => {
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onConfirm} />;
+};
+
+const ModalOverlay = (props) => {
   return (
-    <React.Fragment>
-      <div className={classes.backdrop} onClick={props.onConfirm} />
-      <Card className={classes.modal}>
+    <Card className={classes.modal}>
         <header className={classes.header}>
           <h2>{props.title}</h2>
         </header>
@@ -20,6 +22,15 @@ const ErrorModal = (props) => {
           <Button onClick={props.onConfirm}>Okay</Button>
         </footer>
       </Card>
+  );
+}
+
+const ErrorModal = (props) => {
+  return (
+    <React.Fragment>
+      {}
+
+      
     </React.Fragment>
   );
 };
